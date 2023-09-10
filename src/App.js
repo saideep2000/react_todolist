b4c5dda3c7175d0c336d98953989ba707af9a5d6
saideep samineni <saideepsamineni@saideeps-MacBook-Pro.local>
2023-09-10 16:43:46 -0400

    first commit of TodoList

--- src/App.js
+++ src/App.js
@@ -1,25 +1,19 @@
-import logo from './logo.svg';
-import './App.css';
+import React, {useState} from 'react'
+import TodoList from "./TodoList";
 
 function App() {
+  const [todos, setTodos] = useState([{id: 1, name:'Todo 1' , complete:false}, {id: 2, name:'Todo 2' , complete:true}])
+
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <>
+      <TodoList todos = {todos}/>
+      <input type='text'/>
+      <button>Add Todo</button>
+      <button>Clear Complete</button>
+      <div>0 left to do</div>
+    </>
+  )
 }
 
 export default App;
